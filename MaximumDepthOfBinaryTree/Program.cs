cce5e7111149f2aa6f8250fe4c6b93d56418bb84
SergioF <fs220381@gmail.com>
2020-03-28 10:40:58 +0200

    Добавьте файлы проекта.

---
+++ MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximumDepthOfBinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TreeNode tn1 = new TreeNode(1);
+            TreeNode tn2 = new TreeNode(2);
+            TreeNode tn7 = new TreeNode(7);
+            TreeNode tn4 = new TreeNode(4);
+            tn4.right = tn1;
+            TreeNode tn13 = new TreeNode(13);
+            TreeNode tn11 = new TreeNode(11);
+            tn11.left = tn7;
+            tn11.right = tn2;
+            TreeNode tn8 = new TreeNode(8);
+            tn8.right = tn4;
+            tn8.left = tn13;
+            TreeNode tn4_2 = new TreeNode(4);
+            tn4_2.left = tn11;
+            TreeNode tn5 = new TreeNode(5);
+            tn5.left = tn4_2;
+            tn5.right = tn8;
+
+            TreeNode root = new TreeNode(1);
+            root.left = new TreeNode(2);
+            root.right = new TreeNode(2);
+            //root.left.left = new TreeNode(3);
+            //root.left.right = new TreeNode(3);
+            root.right.left = new TreeNode(3);
+            root.right.right = new TreeNode(3);
+            //root.left.left.left = new TreeNode(4);
+            //root.left.left.right = new TreeNode(4);
+            //root.left.right.left = new TreeNode(4);
+            //root.left.right.right = new TreeNode(4);
+            //root.right.left.left = new TreeNode(4);
+            //root.right.left.right = new TreeNode(4);
+            //root.right.right.left = new TreeNode(4);
+            //root.right.right.right = new TreeNode(4);
+            //root.left.left.left.left = new TreeNode(5);
+            //root.left.left.left.right = new TreeNode(5);
+
+            Solution.MinDepth(root);
+
+            //Solution.IsBalanced(root);
+
+            //Solution.MaxDepth(temp);
+
+            //Solution.HasPathSum(tn4, 4);
+        }
+    }
+}
---
+++ MaximumDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MaximumDepthOfBinaryTree
+{
+    public class Solution
+    {
+        public static int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var stack = new Stack<(TreeNode node, int depth)>();
+            stack.Push((root, 1));
+            var maxDepth = 1;
+
+            while (stack.Count > 0)
+            {
+                var (node, depth) = stack.Pop();
+                if (depth > maxDepth) maxDepth = depth;
+                if (node.left != null) stack.Push((node.left, depth + 1));
+                if (node.right != null) stack.Push((node.right, depth + 1));
+            }
+            return maxDepth;
+        }
+
+        public static bool HasPathSum(TreeNode root, int sum)
+        {
+            if (root == null) return false;
+
+            var nodeStack = new Stack<(TreeNode, int)>();
+            nodeStack.Push((root, root.val));
+
+            while (nodeStack.Count > 0)
+            {
+
+                var temp = nodeStack.Pop();
+                TreeNode node = temp.Item1;
+                var localSum = temp.Item2;
+
+                if (node.left != null) nodeStack.Push((node.left, localSum + node.left.val));
+                if (node.right != null) nodeStack.Push((node.right, localSum + node.right.val));
+
+                if (node.left == null && node.right == null && localSum == sum) return true;
+
+            }
+
+            return false;
+        }
+
+        public static bool IsBalanced(TreeNode root)
+        {
+            if (root == null || (root.left == null && root.right == null)) return true;
+
+            var nodeStack = new Stack<(TreeNode, int)>();
+            nodeStack.Push((root, 1));
+
+            var minimum_level = int.MaxValue;
+            var maximum_level = 0;
+
+            var left_height = 0;
+            var right_height = 0;
+
+            while (nodeStack.Count > 0)
+            {
+                var temp_item = nodeStack.Pop();
+                var node = temp_item.Item1;
+                var height = temp_item.Item2;
+
+                //if (node.left != null) nodeStack.Push(node.left le)
+
+                //if (node.left == null && node.right == null && nodeStack.Count != 0)
+                //    minimum_level = level < minimum_level ? level : minimum_level;
+                //if (node.left == null && node.right == null && nodeStack.Count == 0)
+                //    maximum_level = level;
+
+                //if (node.left != null) nodeStack.Push((node.left, level + 1));
+                //if (node.right != null) nodeStack.Push((node.right, level + 1));
+            }
+
+            minimum_level = minimum_level == int.MaxValue ? 1 : minimum_level;
+            return Math.Abs(maximum_level - minimum_level) > 1 ? false : true;
+        }
+
+        public static int MinDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            var nodeStack = new Stack<(TreeNode, int)>();
+
+            nodeStack.Push((root, 1));
+            var min_depth = int.MaxValue;
+
+            while (nodeStack.Count > 0)
+            {
+                var temp = nodeStack.Pop();
+                var node = temp.Item1;
+                var depth = temp.Item2;
+
+                if (node.left == null && node.right == null)
+                {
+                    min_depth = depth < min_depth ? depth : min_depth;
+                }
+
+                if (node.left != null) nodeStack.Push((node.left, depth + 1));
+                if (node.right != null) nodeStack.Push((node.right, depth + 1));
+            }
+
+            return min_depth;
+        }
+
+        public static bool IsSymmetric(TreeNode root)
+        {
+            if (root == null) return false;
+            if (root.left == null && root.right == null) return true;
+
+            var left_nodeStack = new Stack<TreeNode>();
+            var right_nodeStack = new Stack<TreeNode>();
+
+            left_nodeStack.Push(root.left);
+            right_nodeStack.Push(root.right);
+
+            while(left_nodeStack.Count > 0)
+            {
+
+            }
+            return true;
+        }
+    }
+}
